ab9bd3061758e82d0a2217da0a941e1c71526189
Eleonor <eleonortiamartin@gmail.com>
2025-02-06 10:15:05 -0500

    update

---
+++ src/main/java/VerticalRug.java
@@ -0,0 +1,29 @@
+package main.java;
+
+public class VerticalRug {
+    public static void main(String[] args){
+        String[][] emoji = new String[21][21];
+
+        for (int i = 0; i < emoji.length; i++){
+            for (int j = 0; j < emoji[i].length; j++) {
+                if (j % 2 == 0) {
+                    emoji[i][j] = "🌿"; 
+                }
+                else{
+                emoji[i][j] = "🍓";
+                }
+            }
+        }
+        print2DArray(emoji);
+    }
+
+    public static void print2DArray(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+
+            }
+            System.out.println();
+        }
+    }
+}
